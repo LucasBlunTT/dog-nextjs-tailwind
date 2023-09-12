913f7bffc2dad14ed8521d4daafb3b8b559cb9de
Lucas da Silva <sklucassilva@gmail.com>
2023-09-12 18:36:42 -0300

    botão mobile criado

---
+++ src/components/ButtonHamburguer/index.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+function ButtonHamburguer() {
+  const [colorHover, setColorHover] = useState('#000');
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [activeButton, setActiveButton] = useState(false);
+
+  function handleActive() {
+    setMobileMenu(!mobileMenu);
+    setActiveButton(!activeButton);
+  }
+
+  return (
+    <button
+      className="bg-[#eee] rounded-[0.2rem] h-10 w-10 flex items-center justify-center border-[1px] border-solid border-transparent cursor-pointer hover:bg-white focus:bg-white hover:shadow-[0_0_0_3px_#eee] hover:border-[#fb1] focus:shadow-[0_0_0_3px_#fb1] focus:border-[#fb1] outline-none"
+      onMouseOver={() => setColorHover('#fb1')}
+      onMouseOut={() => setColorHover('#000')}
+      onClick={handleActive}
+    >
+      {activeButton ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke={colorHover}
+          stroke-width="2.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-more-horizontal"
+        >
+          <circle cx="12" cy="12" r="1" />
+          <circle cx="19" cy="12" r="1" />
+          <circle cx="5" cy="12" r="1" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke={colorHover}
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-menu"
+        >
+          <line x1="4" x2="20" y1="12" y2="12" />
+          <line x1="4" x2="20" y1="6" y2="6" />
+          <line x1="4" x2="20" y1="18" y2="18" />
+        </svg>
+      )}
+    </button>
+  );
+}
+
+export default ButtonHamburguer;
--- src/components/HeaderUserNav/index.jsx
+++ src/components/HeaderUserNav/index.jsx
@@ -1,20 +1,24 @@
 'use client';
 
+import Image from 'next/image';
 import React, { useContext, useState } from 'react';
 import Link from 'next/link';
 import { UserContext } from '@/context/UserContext';
 import Feed from '@/assets/feed.svg';
+import ButtonHamburguer from '@/components/ButtonHamburguer';
 import Estatisticas from '@/assets/estatisticas.svg';
 import AdicionarFoto from '@/assets/adicionar.svg';
 import Sair from '@/assets/sair.svg';
-import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
+import useMedia from '@/Hooks/useMedia';
 
 export default function HeaderUserNav() {
-  const [mobile, setMobile] = useState(null);
   const { userLogout } = useContext(UserContext);
   const currentRoute = usePathname();
   const router = useRouter();
+  const mobile = useMedia('(max-width: 40rem)');
+
+  console.log(mobile);
 
   function handleLogout() {
     userLogout();
@@ -22,47 +26,50 @@ export default function HeaderUserNav() {
   }
 
   return (
-    <nav className="grid grid-cols-4 gap-4">
-      <Link
-        className={`bg-[#eee] rounded-[0.2rem] h-10 w-10 flex items-center justify-center border-[1px] border-solid border-transparent cursor-pointer hover:bg-white focus:bg-white hover:shadow-[0_0_0_3px_#eee] hover:border-[#333] focus:shadow-[0_0_0_3px_#eee] focus:border-[#fb1] outline-none ${
-          currentRoute == '/user'
-            ? 'bg-white shadow-[0_0_0_3px_#fea] border-[#fb1]'
-            : ''
-        }`}
-        href={'/user'}
-      >
-        <Image src={Feed} alt="feed" />
-        {mobile && 'Minhas Fotos'}
-      </Link>
-      <Link
-        className={`bg-[#eee] rounded-[0.2rem] h-10 w-10 flex items-center justify-center border-[1px] border-solid border-transparent cursor-pointer hover:bg-white focus:bg-white hover:shadow-[0_0_0_3px_#eee] hover:border-[#333] focus:shadow-[0_0_0_3px_#eee] focus:border-[#fb1] outline-none ${
-          currentRoute == '/user/estatisticas'
-            ? 'bg-white shadow-[0_0_0_3px_#fea] border-[#fb1]'
-            : ''
-        }`}
-        href={'/user/estatisticas'}
-      >
-        <Image src={Estatisticas} alt="estatisticas" />
-        {mobile && 'Estatisticas'}
-      </Link>
-      <Link
-        className={`bg-[#eee] rounded-[0.2rem] h-10 w-10 flex items-center justify-center border-[1px] border-solid border-transparent cursor-pointer hover:bg-white focus:bg-white hover:shadow-[0_0_0_3px_#eee] hover:border-[#333] focus:shadow-[0_0_0_3px_#eee] focus:border-[#fb1] outline-none ${
-          currentRoute == '/user/postar'
-            ? 'bg-white shadow-[0_0_0_3px_#fea] border-[#fb1]'
-            : ''
-        }`}
-        href={'/user/postar'}
-      >
-        <Image src={AdicionarFoto} alt="adicionar fotos" />
-        {mobile && 'Adicionar Foto'}
-      </Link>
-      <button
-        className="bg-[#eee] rounded-[0.2rem] h-10 w-10 flex items-center justify-center border-[1px] border-solid border-transparent cursor-pointer hover:bg-white focus:bg-white hover:shadow-[0_0_0_3px_#eee] hover:border-[#333] focus:shadow-[0_0_0_3px_#eee] focus:border-[#fb1] outline-none"
-        onClick={handleLogout}
-      >
-        <Image src={Sair} alt="sair" />
-        {mobile && 'Sair'}
-      </button>
-    </nav>
+    <>
+      {mobile && <ButtonHamburguer />}
+      <nav className="grid grid-cols-4 gap-4">
+        <Link
+          className={`bg-[#eee] rounded-[0.2rem] h-10 w-10 flex items-center justify-center border-[1px] border-solid border-transparent cursor-pointer hover:bg-white focus:bg-white hover:shadow-[0_0_0_3px_#eee] hover:border-[#333] focus:shadow-[0_0_0_3px_#eee] focus:border-[#fb1] outline-none ${
+            currentRoute == '/user'
+              ? 'bg-white shadow-[0_0_0_3px_#fea] border-[#fb1]'
+              : ''
+          }`}
+          href={'/user'}
+        >
+          <Image src={Feed} alt="feed" />
+          {mobile && 'Minhas Fotos'}
+        </Link>
+        <Link
+          className={`bg-[#eee] rounded-[0.2rem] h-10 w-10 flex items-center justify-center border-[1px] border-solid border-transparent cursor-pointer hover:bg-white focus:bg-white hover:shadow-[0_0_0_3px_#eee] hover:border-[#333] focus:shadow-[0_0_0_3px_#eee] focus:border-[#fb1] outline-none ${
+            currentRoute == '/user/estatisticas'
+              ? 'bg-white shadow-[0_0_0_3px_#fea] border-[#fb1]'
+              : ''
+          }`}
+          href={'/user/estatisticas'}
+        >
+          <Image src={Estatisticas} alt="estatisticas" />
+          {mobile && 'Estatisticas'}
+        </Link>
+        <Link
+          className={`bg-[#eee] rounded-[0.2rem] h-10 w-10 flex items-center justify-center border-[1px] border-solid border-transparent cursor-pointer hover:bg-white focus:bg-white hover:shadow-[0_0_0_3px_#eee] hover:border-[#333] focus:shadow-[0_0_0_3px_#eee] focus:border-[#fb1] outline-none ${
+            currentRoute == '/user/postar'
+              ? 'bg-white shadow-[0_0_0_3px_#fea] border-[#fb1]'
+              : ''
+          }`}
+          href={'/user/postar'}
+        >
+          <Image src={AdicionarFoto} alt="adicionar fotos" />
+          {mobile && 'Adicionar Foto'}
+        </Link>
+        <button
+          className="bg-[#eee] rounded-[0.2rem] h-10 w-10 flex items-center justify-center border-[1px] border-solid border-transparent cursor-pointer hover:bg-white focus:bg-white hover:shadow-[0_0_0_3px_#eee] hover:border-[#333] focus:shadow-[0_0_0_3px_#eee] focus:border-[#fb1] outline-none"
+          onClick={handleLogout}
+        >
+          <Image src={Sair} alt="sair" />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   );
 }
