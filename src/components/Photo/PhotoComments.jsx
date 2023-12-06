c4d62ece827395244422031eaec31d0dbc7e65bc
Lucas da Silva <sklucassilva@gmail.com>
2023-12-06 18:05:05 -0300

    componente comentário compelto

--- src/components/Photo/PhotoComments.jsx
+++ src/components/Photo/PhotoComments.jsx
@@ -1,11 +1,24 @@
 'use client';
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import PhotoCommentsForm from './PhotoCommentsForm';
 import { UserContext } from '@/context/UserContext';
 
-export default function PhotoComments() {
+export default function PhotoComments(props) {
+  const [comments, setComments] = useState(() => props.comments);
   const { login } = useContext(UserContext);
 
-  return <div>{login && <PhotoCommentsForm />}</div>;
+  return (
+    <>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
+    </>
+  );
 }
--- src/components/Photo/PhotoCommentsForm.jsx
+++ src/components/Photo/PhotoCommentsForm.jsx
@@ -1,9 +1,39 @@
-'use client'
+'use client';
 
-import React from 'react'
+import React, { useState } from 'react';
+import Enviar from '@/assets/enviar.svg';
+import Image from 'next/image';
+import { COMMENT_POST } from '@/api/api';
+import useFetch from '@/Hooks/useFetch';
+import Error from '../Error';
 
-export default function PhotoCommentsForm() {
+export default function PhotoCommentsForm({ id, setComments }) {
+  const [comment, setComment] = useState('');
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
+  }
   return (
-    <div>PhotoCommentsForm</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={(event) => setComment(event.target.value)}
+      />
+
+      <button>
+        <Image src={Enviar} alt="enviar" />
+      </button>
+      <Error error={error} />
+    </form>
+  );
 }
